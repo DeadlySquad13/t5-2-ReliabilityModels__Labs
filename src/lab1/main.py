4c54d10bc69f1be7ec5ef90cf0bf8279b72038e5
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-02-12 16:26:19 +0300

    [Enh]: Added jupynium suppor.

--- src/lab1/main.py
+++ src/lab1/main.py
@@ -18,7 +18,6 @@ def get_value_in_range(
     p: Possibilities
 ):
     border = 0
-    print(v, p)
 
     for i, p_i in enumerate(p):
         border = border + p_i
@@ -43,7 +42,6 @@ def walk(initial_state, times, transition_matrix):
         return(initial_state)
 
     next_state = make_step(initial_state, transition_matrix)
-    print(next_state)
 
     return walk(next_state, times - 1, transition_matrix)
 
@@ -61,28 +59,7 @@ def simulate_walks(
             for _ in range(number_of_experiments)]
 
 
-def visualize_states():
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    plt.style.use('_mpl-gallery')
-
-    # make data:
-    x = 0.5 + np.arange(8)
-    y = [4.8, 5.5, 3.5, 4.6, 6.5, 6.6, 2.6, 3.0]
-
-    # plot
-    fig, ax = plt.subplots()
-
-    ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
-
-    ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-           ylim=(0, 8), yticks=np.arange(1, 8))
-
-    plt.show()
-
-
-def main():
+def get_results():
     transition_matrix = [
         [0.3, 0.3, 0.4],
         [0.5, 0.3, 0.2],
@@ -90,9 +67,12 @@ def main():
     ]
     initial_state = 0
     results = simulate_walks(initial_state, 2, transition_matrix)
-    # print(results)
-    print(Counter(results))
-    # visualize_states()
+
+    return Counter(results)
+
+
+def main():
+    print(get_results())
 
 
 if __name__ == '__main__':
